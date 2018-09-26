ac14714b8d46657faf75fce0e21eac7bedbc3bb4
William Lin <wlsaidhi@gmail.com>
2018-09-26 17:22:22 -0500

    remo

--- setup.sh
+++ setup.sh
@@ -11,20 +11,6 @@ setup() {
 }
 
 install_packages() {
-    xargs -a <(grep '^\s*[^#]' "${1}") -- sudo apt-get install -y
-}
-
-usage() {
-    cat <<EOF
-Usage: ${0##*/} [options] module ...
-
-    automatically set up system 'module' using configured dotfiles
-
-    options:
-        -c  checkout repo submodules before taking any action
-        -f  force overwrite a dotfile that already exists
-EOF
-}
 
 bad_usage() {
     usage 1>&2
@@ -53,56 +39,3 @@ main() {
     done
 
     # get module names
-    local modules=( "$@" )
-    [ ${#modules[@]} -eq 0 ] && bad_usage
-
-    # get base dir location
-    local base_dir="${HOME}/dotfiles"
-    local target_dir="$base_dir/.."
-
-    # setup system
-    local global_pkg_file="$base_dir/packages"
-    setup && install_packages "$global_pkg_file" ||
-        error "system setup failed"
-
-    # checkout submodules if needed
-    if $checkout; then
-        git submodule update --init --recursive
-    fi
-
-    # set up modules
-    for module in "${modules[@]}"; do
-        # ensure valid module
-        [ -d "$module" ] || error "module: $module does not exist"
-
-        # run setup script if any
-        local setup_script="${module}_setup.sh"
-        if [ -x "$setup_script" ]; then
-            "$base_dir/$setup_script" || error "script failed: $setup_script"
-        fi
-
-        # install package requiements if any
-        local package_file="${module}_packages"
-        if [ -e "$package_file" ]; then
-            install_packages "$package_file" ||
-                error "package installation failed"
-        fi
-
-        # if force set, delete any possible conflicts
-        if $force; then
-            for file in $(ls -a "$module"); do
-                [ "$file" = "." -o "$file" = ".." ] && continue;
-                local path="$target_dir/$file"
-                echo "warning, removing: $file"
-                rm -rf "$path"
-            done
-        fi
-
-        # stow the module
-        stow "$module" || error "stow failed for module: $module"
-    done
-
-    return 0
-}
-
-main "$@"
